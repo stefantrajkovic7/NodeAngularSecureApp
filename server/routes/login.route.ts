a9efe689935082d478d80d630e5d25135465c0ea
saibot777 <stefantrajkovic777@gmail.com>
2018-03-12 12:16:07 +0100

    completeted login on Backend and UI

--- server/routes/login.route.ts
+++ server/routes/login.route.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
-import {sessionStore} from "../core/session-store";
+import * as argon from 'argon2';
 import {db} from "../core/database";
 import {IUser} from "../models/db-user.model";
+import {sessionStore} from "../core/session-store";
+import {randomBytes} from "../helpers/security.utils";
 
 export function login(req: Request, res: Response) {
   const credentials = req.body;
@@ -11,15 +13,35 @@ export function login(req: Request, res: Response) {
   if (!user) {
     res.sendStatus(403);
   } else {
-    attemptLogin(credentials, user)
-      .then(() => {
+    loginAndBuildResponse(credentials, user, res);
+  }
 
-      })
-      .catch(() => res.sendStatus(403));
+}
+
+async function loginAndBuildResponse(credentials: any, user: IUser, res: Response) {
+
+  try {
+    const sessionId = await attemptLogin(credentials, user);
+
+    res.cookie("SESSIONID", sessionId, { httpOnly: true });
+    res.status(200).json({id: user.id, email: user.email});
+  } catch (err) {
+    res.sendStatus(403);
   }
 
 }
 
 async function attemptLogin(credentials: any, user: IUser) {
+  const isPasswordValid = await argon.verify(user.passwordDigest,
+                                            credentials.password);
 
+  if (!isPasswordValid) {
+    throw new Error("Password Invalid");
+  }
+
+  const sessionId = await randomBytes(32).then(bytes => bytes.toString('hex'));
+
+  sessionStore.createSession(sessionId, user);
+
+  return sessionId;
 }
